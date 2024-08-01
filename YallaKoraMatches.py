df2ec8821f6945337eaeceb388df85b2f810f134
SIFY <154904386+Sify47@users.noreply.github.com>
2024-08-01 16:43:18 +0300

    Update YallaKoraMatches.py

--- YallaKoraMatches.py
+++ YallaKoraMatches.py
@@ -9,3 +9,18 @@ animal = st.text_input('Type an animal')
 if st.button('Check availability'):
     have_it = animal.lower() in animal_shelter
     'We have that animal!' if have_it else 'We don\'t have that animal.'
+
+
+color = st.select_slider(
+    "Select a color of the rainbow",
+    options=[
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "indigo",
+        "violet",
+    ],
+)
+st.write("My favorite color is", color)
