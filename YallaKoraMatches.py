3e4f11159da5b9ff9f3fe34c834b716f906c76ff
SIFY <154904386+Sify47@users.noreply.github.com>
2024-08-01 16:42:09 +0300

    Update YallaKoraMatches.py

--- YallaKoraMatches.py
+++ YallaKoraMatches.py
@@ -2,3 +2,10 @@ import streamlit as st
 import pandas as pd
 
 st.write("Hi")
+animal_shelter = ['cat', 'dog', 'rabbit', 'bird']
+
+animal = st.text_input('Type an animal')
+
+if st.button('Check availability'):
+    have_it = animal.lower() in animal_shelter
+    'We have that animal!' if have_it else 'We don\'t have that animal.'
