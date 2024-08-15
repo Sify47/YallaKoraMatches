caa16301d6aaabbc670d84a76a626aa6fcd9a38c
SIFY <154904386+Sify47@users.noreply.github.com>
2024-08-15 23:08:32 +0300

    Update YallaKoraMatches.py

--- YallaKoraMatches.py
+++ YallaKoraMatches.py
@@ -1,26 +1,51 @@
-import streamlit as st
+import psycopg2
+import psycopg2.extras
+from sqlalchemy import create_engine
+import requests
 import pandas as pd
+from bs4 import BeautifulSoup
+from tabulate import tabulate
+import streamlit as st
 
-st.write("Hi")
-animal_shelter = ['cat', 'dog', 'rabbit', 'bird']
+connect_sql = psycopg2.connect(host = "localhost" , dbname = "postgres" , port= 5432 , user = "postgres", password = "01141117352")
+cur = connect_sql.cursor(cursor_factory=psycopg2.extras.DictCursor)
+conn_eng = create_engine("postgresql://postgres:01141117352@localhost:5432/postgres")
+con = conn_eng.connect()
 
-animal = st.text_input('Type an animal')
+def commit():
+    connect_sql.commit()
+def close():
+    connect_sql.close()
 
-if st.button('Check availability'):
-    have_it = animal.lower() in animal_shelter
-    'We have that animal!' if have_it else 'We don\'t have that animal.'
+# res = requests.get(f"https://www.yallakora.com/match-center?date=8/14/2024")
+res = requests.get(f"https://www.yallakora.com/match-center")
+src = res.content
+soup = BeautifulSoup(src , "lxml")
 
+chamption = soup.find_all("div" , {"class" : "matchCard"})
+mat = []
 
-color = st.select_slider(
-    "Select a color of the rainbow",
-    options=[
-        "red",
-        "orange",
-        "yellow",
-        "green",
-        "blue",
-        "indigo",
-        "violet",
-    ],
-)
-st.write("My favorite color is", color)
+def get(chamption) : 
+    chamption_title = chamption.contents[1].find("h2").text.strip()
+    all = chamption.contents[3].find_all("div" , {"class":"item"})
+    num = len(all)
+    for i in range(num):
+        asb = all[i].find("div" , {"class":"date"}).text.strip()
+        state = all[i].find("span").text.strip()
+        resl = all[i].find("div" , {"class" : "MResult"}).find_all("span" , {"class" : "score"})
+        reslt = f"{resl[0].text.strip()} - {resl[1].text.strip()}"
+        clock = all[i].find("div" , {"class" : "MResult"}).find("span" , {"class" : "time"}).text.strip()
+        teamA = all[i].find("div" , {"class":"teamA"}).text.strip()
+        teamB = all[i].find("div" , {"class":"teamB"}).text.strip()
+        mat.append({"اسم البطوله":chamption_title ,"الفريق الاول": teamA ,"النتيحه" : reslt, "الفريق الثانى": teamB , "التوقيت":clock, "الاسبوع":  asb, "الحاله": state})
+
+for i in range(len(chamption)) :
+    get(chamption[i])
+    
+df = pd.DataFrame(mat)
+df.to_sql("matches" , con , if_exists="replace")
+cur.execute("SELECT * FROM matches")
+x = cur.fetchall()
+print(tabulate(x,tablefmt="psql"))
+
+st.write(x)
